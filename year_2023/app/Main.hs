ea0af856f41835e81042068e7c566787177cd5ba
James Green <JamesTadleyGreen@outlook.com>
2023-12-09 23:08:28 +0000

    Day 7 and 8

--- year_2023/app/Main.hs
+++ year_2023/app/Main.hs
@@ -6,7 +6,9 @@ import Days.Day02
 import Days.Day03
 import Days.Day04
 import Days.Day05
+import Days.Day07
+import Days.Day08
 
 main :: IO ()
 -- main = defaultMain [bench "test" $ nfIO (print day04)]
-main = print day05
+main = print day08
